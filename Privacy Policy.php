9ba3a548ad1d9442336a814c8377294d59599b97
lom0707 <lom1325322@gmail.com>
2025-06-08 06:47:25 +0700

    Update Privacy Policy.php

--- Privacy Policy.php
+++ Privacy Policy.php
@@ -1,69 +1,53 @@
-Privacy Policy
-This privacy policy applies to the Vpn G net app (hereby referred to as "Application") for mobile devices that was created by Vpn G net (hereby referred to as "Service Provider") as a Free service. This service is intended for use "AS IS".
+นโยบายความเป็นส่วนตัว
+นโยบายความเป็นส่วนตัวนี้ใช้กับแอป G NET VPN (ต่อไปนี้เรียกว่า "แอปพลิเคชัน") สำหรับอุปกรณ์พกพาที่สร้างโดย G NET VPN (ต่อไปนี้เรียกว่า "ผู้ให้บริการ") ในฐานะบริการฟรี บริการนี้มีวัตถุประสงค์เพื่อใช้ "ตามที่เป็น"
 
+การรวบรวมและการใช้ข้อมูล
+แอปพลิเคชันจะรวบรวมข้อมูลเมื่อคุณดาวน์โหลดและใช้งาน ข้อมูลนี้อาจรวมถึงข้อมูล เช่น
 
-Information Collection and Use
-The Application collects information when you download and use it. This information may include information such as
+ที่อยู่โปรโตคอลอินเทอร์เน็ตของอุปกรณ์ของคุณ (เช่น ที่อยู่ IP)
+หน้าต่างๆ ของแอปพลิเคชันที่คุณเข้าชม เวลาและวันที่ที่คุณเข้าชม เวลาที่ใช้ในหน้าเหล่านั้น
+เวลาที่ใช้ในแอปพลิเคชัน
+ระบบปฏิบัติการที่คุณใช้บนอุปกรณ์พกพาของคุณ
 
-Your device's Internet Protocol address (e.g. IP address)
-The pages of the Application that you visit, the time and date of your visit, the time spent on those pages
-The time spent on the Application
-The operating system you use on your mobile device
+แอปพลิเคชันไม่รวบรวมข้อมูลที่ชัดเจนเกี่ยวกับตำแหน่งของอุปกรณ์พกพาของคุณ
 
-The Application does not gather precise information about the location of your mobile device.
+ผู้ให้บริการอาจใช้ข้อมูลที่คุณให้มาเพื่อติดต่อคุณเป็นครั้งคราวเพื่อแจ้งข้อมูลสำคัญ ประกาศที่จำเป็น และโปรโมชั่นทางการตลาดให้คุณทราบ
 
+เพื่อประสบการณ์ที่ดีขึ้น ขณะใช้แอปพลิเคชัน ผู้ให้บริการอาจขอให้คุณให้ข้อมูลส่วนบุคคลบางอย่างแก่เรา  ข้อมูลที่ผู้ให้บริการร้องขอจะถูกเก็บรักษาไว้โดยผู้ให้บริการและใช้ตามที่อธิบายไว้ในนโยบายความเป็นส่วนตัวนี้
 
-The Service Provider may use the information you provided to contact you from time to time to provide you with important information, required notices and marketing promotions.
+การเข้าถึงของบุคคลที่สาม
+ข้อมูลรวมที่ไม่ระบุตัวตนเท่านั้นที่จะถูกส่งต่อไปยังบริการภายนอกเป็นระยะๆ เพื่อช่วยให้ผู้ให้บริการปรับปรุงแอปพลิเคชันและบริการของตน ผู้ให้บริการอาจแบ่งปันข้อมูลของคุณกับบุคคลที่สามในลักษณะที่อธิบายไว้ในคำชี้แจงความเป็นส่วนตัวนี้
 
+โปรดทราบว่าแอปพลิเคชันใช้บริการของบุคคลที่สามซึ่งมีนโยบายความเป็นส่วนตัวของตนเองเกี่ยวกับการจัดการข้อมูล ด้านล่างนี้คือลิงก์ไปยังนโยบายความเป็นส่วนตัวของผู้ให้บริการบุคคลที่สามที่แอปพลิเคชันใช้:
 
-For a better experience, while using the Application, the Service Provider may require you to provide us with certain personally identifiable information. The information that the Service Provider request will be retained by them and used as described in this privacy policy.
+บริการ Google Play
 
+ผู้ให้บริการอาจเปิดเผยข้อมูลที่ผู้ใช้ให้มาและรวบรวมโดยอัตโนมัติ:
 
-Third Party Access
-Only aggregated, anonymized data is periodically transmitted to external services to aid the Service Provider in improving the Application and their service. The Service Provider may share your information with third parties in the ways that are described in this privacy statement.
+ตามที่กฎหมายกำหนด เช่น เพื่อปฏิบัติตามหมายเรียกหรือกระบวนการทางกฎหมายที่คล้ายคลึงกัน;
+เมื่อพวกเขาเชื่อโดยสุจริตใจว่าการเปิดเผยเป็นสิ่งจำเป็นในการปกป้องสิทธิ์ของพวกเขา ปกป้องความปลอดภัยของคุณหรือความปลอดภัยของผู้อื่น สืบสวนการฉ้อโกง หรือตอบสนองต่อคำขอของรัฐบาล;  กับผู้ให้บริการที่เชื่อถือได้ซึ่งทำงานในนามของพวกเขา จะไม่มีการใช้งานข้อมูลที่เราเปิดเผยให้พวกเขาอย่างอิสระ และได้ตกลงที่จะปฏิบัติตามกฎที่กำหนดไว้ในคำชี้แจงความเป็นส่วนตัวนี้
 
+สิทธิ์ในการไม่ยินยอม
+คุณสามารถหยุดการรวบรวมข้อมูลทั้งหมดโดยแอปพลิเคชันได้อย่างง่ายดายโดยการถอนการติดตั้ง คุณสามารถใช้กระบวนการถอนการติดตั้งมาตรฐานที่มีให้เป็นส่วนหนึ่งของอุปกรณ์มือถือของคุณหรือผ่านทางตลาดแอปพลิเคชันมือถือหรือเครือข่าย
 
-Please note that the Application utilizes third-party services that have their own Privacy Policy about handling data. Below are the links to the Privacy Policy of the third-party service providers used by the Application:
+นโยบายการเก็บรักษาข้อมูล
+ผู้ให้บริการจะเก็บข้อมูลที่ผู้ใช้ให้ไว้ตราบเท่าที่คุณใช้แอปพลิเคชันและเป็นระยะเวลาที่เหมาะสมหลังจากนั้น หากคุณต้องการให้ผู้ให้บริการลบข้อมูลที่ผู้ใช้ให้ไว้ซึ่งคุณให้ไว้ผ่านแอปพลิเคชัน โปรดติดต่อที่ lom132532@gmail.com และผู้ให้บริการจะตอบกลับภายในเวลาที่เหมาะสม
 
-Google Play Services
+ เด็ก
+ผู้ให้บริการจะไม่ใช้แอปพลิเคชันเพื่อขอข้อมูลหรือทำการตลาดกับเด็กอายุต่ำกว่า 13 ปีโดยเจตนา
 
-The Service Provider may disclose User Provided and Automatically Collected Information:
+แอปพลิเคชันนี้ไม่ได้มุ่งเป้าไปที่ใครก็ตามที่มีอายุต่ำกว่า 13 ปี ผู้ให้บริการจะไม่รวบรวมข้อมูลส่วนตัวที่ระบุตัวตนของเด็กอายุต่ำกว่า 13 ปีโดยเจตนา ในกรณีที่ผู้ให้บริการพบว่าเด็กอายุต่ำกว่า 13 ปีได้ให้ข้อมูลส่วนบุคคล ผู้ให้บริการจะลบข้อมูลดังกล่าวออกจากเซิร์ฟเวอร์ทันที หากคุณเป็นผู้ปกครองหรือผู้ดูแล และคุณทราบว่าบุตรหลานของคุณได้ให้ข้อมูลส่วนบุคคลแก่เรา โปรดติดต่อผู้ให้บริการ (lom132532@gmail.com) เพื่อให้พวกเขาสามารถดำเนินการที่จำเป็นได้
 
-as required by law, such as to comply with a subpoena, or similar legal process;
-when they believe in good faith that disclosure is necessary to protect their rights, protect your safety or the safety of others, investigate fraud, or respond to a government request;
-with their trusted services providers who work on their behalf, do not have an independent use of the information we disclose to them, and have agreed to adhere to the rules set forth in this privacy statement.
+ความปลอดภัย
+ผู้ให้บริการมีความกังวลเกี่ยวกับการปกป้องความลับของข้อมูลของคุณ ผู้ให้บริการจัดให้มีการป้องกันทางกายภาพ อิเล็กทรอนิกส์ และตามขั้นตอน เพื่อปกป้องข้อมูลที่ผู้ให้บริการประมวลผลและดูแล
 
-Opt-Out Rights
-You can stop all collection of information by the Application easily by uninstalling it. You may use the standard uninstall processes as may be available as part of your mobile device or via the mobile application marketplace or network.
+การเปลี่ยนแปลง
+นโยบายความเป็นส่วนตัวนี้อาจได้รับการอัปเดตเป็นครั้งคราวด้วยเหตุผลใดก็ตาม  ผู้ให้บริการจะแจ้งให้คุณทราบถึงการเปลี่ยนแปลงใดๆ ของนโยบายความเป็นส่วนตัวโดยการอัปเดตหน้านี้ด้วยนโยบายความเป็นส่วนตัวฉบับใหม่ เราขอแนะนำให้คุณตรวจสอบนโยบายความเป็นส่วนตัวนี้เป็นประจำสำหรับการเปลี่ยนแปลงใดๆ เนื่องจากการใช้งานต่อเนื่องถือเป็นการอนุมัติการเปลี่ยนแปลงทั้งหมด
 
+นโยบายความเป็นส่วนตัวนี้มีผลบังคับใช้ตั้งแต่วันที่ 7 มิถุนายน 2025
 
-Data Retention Policy
-The Service Provider will retain User Provided data for as long as you use the Application and for a reasonable time thereafter. If you'd like them to delete User Provided Data that you have provided via the Application, please contact them at lom132532@gmail.com and they will respond in a reasonable time.
+ความยินยอมของคุณ
+การใช้แอปพลิเคชันแสดงว่าคุณยินยอมให้มีการประมวลผลข้อมูลของคุณตามที่ระบุไว้ในนโยบายความเป็นส่วนตัวนี้และตามที่เราแก้ไขเพิ่มเติม
 
-
-Children
-The Service Provider does not use the Application to knowingly solicit data from or market to children under the age of 13.
-
-
-The Application does not address anyone under the age of 13. The Service Provider does not knowingly collect personally identifiable information from children under 13 years of age. In the case the Service Provider discover that a child under 13 has provided personal information, the Service Provider will immediately delete this from their servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact the Service Provider (lom132532@gmail.com) so that they will be able to take the necessary actions.
-
-
-Security
-The Service Provider is concerned about safeguarding the confidentiality of your information. The Service Provider provides physical, electronic, and procedural safeguards to protect information the Service Provider processes and maintains.
-
-
-Changes
-This Privacy Policy may be updated from time to time for any reason. The Service Provider will notify you of any changes to the Privacy Policy by updating this page with the new Privacy Policy. You are advised to consult this Privacy Policy regularly for any changes, as continued use is deemed approval of all changes.
-
-
-This privacy policy is effective as of 2025-06-02
-
-
-Your Consent
-By using the Application, you are consenting to the processing of your information as set forth in this Privacy Policy now and as amended by us.
-
-
-Contact Us
-If you have any questions regarding privacy while using the Application, or have questions about the practices, please contact the Service Provider via email at lom132532@gmail.com.
-
-This privacy policy page was generated by App Privacy Policy Generator
+ติดต่อเรา
+หากคุณมีคำถามใดๆ เกี่ยวกับความเป็นส่วนตัวขณะใช้แอปพลิเคชันหรือมีคำถามเกี่ยวกับแนวทางปฏิบัติ โปรดติดต่อผู้ให้บริการทางอีเมลที่ lom132532@gmail.com
